4907a257b7750ad646e3527187010dd558f52212
emily-li <emily.li@live.co.uk>
2017-08-21 01:18:23 +0100

    Adding RandomStockValueGenerator component

--- StockUpdater/src/main/java/com/liemily/stockupdater/RandomStockValueGenerator.java
+++ StockUpdater/src/main/java/com/liemily/stockupdater/RandomStockValueGenerator.java
@@ -1,13 +1,20 @@
 package com.liemily.stockupdater;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
 import java.util.Random;
 
 /**
  * Created by Emily Li on 20/08/2017.
  */
+@Component
+@Lazy
 public class RandomStockValueGenerator implements StockValueGenerator {
     private Random random;
 
+    @Autowired
     public RandomStockValueGenerator(Random random) {
         this.random = random;
     }
