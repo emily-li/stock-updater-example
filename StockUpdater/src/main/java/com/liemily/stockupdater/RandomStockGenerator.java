20e61e202640f925c1bd14ff103f72b8c9839f30
emily-li <emily.li@live.co.uk>
2017-08-22 23:31:48 +0100

    Adding stock generation functionality

--- StockUpdater/src/main/java/com/liemily/stockupdater/RandomStockGenerator.java
+++ StockUpdater/src/main/java/com/liemily/stockupdater/RandomStockGenerator.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
-import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Created by Emily Li on 20/08/2017.
@@ -15,19 +15,16 @@ import java.util.Random;
 @Component
 @Lazy
 public class RandomStockGenerator implements StockGenerator {
-    private Random random;
     private double minValue;
     private double maxValue;
     private int minVol;
     private int maxVol;
 
     @Autowired
-    public RandomStockGenerator(Random random,
-                                @Value("${stockupdater.generator.value.min}") double minValue,
+    public RandomStockGenerator(@Value("${stockupdater.generator.value.min}") double minValue,
                                 @Value("${stockupdater.generator.value.max}") double maxValue,
                                 @Value("${stockupdater.generator.vol.min}") int minVol,
                                 @Value("${stockupdater.generator.vol.max}") int maxVol) {
-        this.random = random;
         this.minValue = minValue;
         this.maxValue = maxValue;
         this.minVol = minVol;
@@ -43,12 +40,12 @@ public class RandomStockGenerator implements StockGenerator {
 
     @Override
     public double generateValue(double min, double max) {
-        return 0;
+        return ThreadLocalRandom.current().nextDouble(minValue, maxValue);
     }
 
     @Override
     public int generateVolume(int min, int max) {
-        return 0;
+        return ThreadLocalRandom.current().nextInt(minVol, maxVol);
     }
 
     @Override
