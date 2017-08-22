d7b6afd6df71842a3ef73da21065084b7fe7635a
emily-li <emily.li@live.co.uk>
2017-08-23 00:11:19 +0100

    Removing unneeded bean

--- StockUpdater/src/main/java/com/liemily/stockupdater/config/StockUpdaterConfig.java
+++ StockUpdater/src/main/java/com/liemily/stockupdater/config/StockUpdaterConfig.java
@@ -1,11 +1,8 @@
 package com.liemily.stockupdater.config;
 
 import com.liemily.stock.config.StockConfig;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-
-import java.util.Random;
 
 /**
  * Created by Emily Li on 21/08/2017.
@@ -13,8 +10,4 @@ import java.util.Random;
 @Configuration
 @Import(StockConfig.class)
 public class StockUpdaterConfig {
-    @Bean
-    Random random() {
-        return new Random();
-    }
 }
