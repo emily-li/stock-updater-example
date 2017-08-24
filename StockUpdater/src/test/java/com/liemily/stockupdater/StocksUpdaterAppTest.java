27a4be9ddd9fba64079d6593d249c59aabf0f0cc
emily-li <emily.li@live.co.uk>
2017-08-24 20:41:58 +0100

    Adding test to verify stocks modulate over time

---
+++ StockUpdater/src/test/java/com/liemily/stockupdater/StocksUpdaterAppTest.java
@@ -0,0 +1,35 @@
+package com.liemily.stockupdater;
+
+import com.liemily.stock.Stock;
+import com.liemily.stock.StockRepository;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+/**
+ * Created by Emily Li on 22/08/2017.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StocksUpdaterAppTest {
+    @Autowired
+    private StockRepository stockRepository;
+
+    @Test
+    public void testStockValueModulatesOverTime() throws Exception {
+        final String symbol = "SYM" + UUID.randomUUID();
+        BigDecimal initialValue = BigDecimal.valueOf(1.23);
+
+        stockRepository.save(new Stock(symbol, initialValue, 1));
+        Thread.sleep(1 * 1000);
+        BigDecimal alteredValue = stockRepository.findOne(symbol).getValue();
+
+        Assert.assertNotEquals(alteredValue, initialValue);
+    }
+}
